2bd71452673ecc64c244c206af195c21d6557679
Kirk Worley <kworley@csumb.edu>
2017-10-11 06:05:31 +0000

    Addition of homework3 in entirety. Ready for push. Homework 3 is a quiz which tells the user what starter pokemon they are based on their answers.

---
+++ labs/lab5/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Lab 5: Technology Checkout</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1> Technology Center: Checkout System </h1>
+        
+        <form>
+            Device: <input type="text">
+        </form>
+    </body>
+</html>
