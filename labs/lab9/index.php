6d1d9d7d78d2259d7e6b0e8b20c1ba01503d7b67
Kirk Worley <kworley@csumb.edu>
2017-12-04 02:25:43 +0000

    Finish lab 9

--- labs/lab9/index.php
+++ labs/lab9/index.php
@@ -1,9 +1,32 @@
 <?php
     include 'inc/header.php';
 ?>
-            
-    <!-- add Carousel component -->
-    <br /><br />
+    <center>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" data-interval="2500">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <img class="d-block img-fluid" src="img/alex.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="img/carl.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="img/charlie.jpg" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="img/sally.jpg" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="img/samantha.jpg" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="img/ted.jpg" alt="Third slide">
+                </div>
+            </div>
+        </div>  
+    </center>
+    
+    <br><br>
     <a class="btn btn-outline-primary" href="adoptions.php" role="button">Adopt Now! </a>
     
     <br /><br />
